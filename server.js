2d3ec40f27bcd70a5e5c659b1e5736e9778fd956
kiran14997 <b.sreekiran14997@gmail.com>
2016-09-27 22:05:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function(req,res) {
-    res.send('this is my first article');
+    res.send('hello is my first article');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
